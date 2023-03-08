f6dd42a91aa5ac1a34634afde50d588fd544ee4f
TimofeyNikulin <104652883+TimofeyNikulin@users.noreply.github.com>
2023-03-08 20:31:12 +0300

    Тренировки в полёте

--- app.py
+++ app.py
@@ -10,6 +10,42 @@ def index():
     return render_template('index.html', title='Заготовка')
 
 
+@app.route('/training/<string:prof>')
+def training(prof):
+    profs = {
+        'engineer_prof': [
+            'инженер-исследователь',
+            'пилот',
+            'строитель',
+            'иженер по терраформированию',
+            'инженер жизнеобеспечения',
+            'оператор марсохода',
+            'киберинженер',
+            'штурман',
+            'пилот дронов'
+        ],
+        'science_prof': [
+            'экзобиолог',
+            'врач',
+            'климатолог',
+            'специалист по радиационной защите',
+            'астрогеолог',
+            'гляциолог',
+            'метеоролог'
+        ]
+    }
+
+    images = {
+        "engineer_prof": 'engineer.jpg',
+        "science_prof": 'science.jpg'
+    }
+
+    if prof in profs['engineer_prof']:
+        return render_template('training.html', img=images['engineer_prof'], title='Инженерные тренажеры')
+    else:
+        return render_template('training.html', img=images['science_prof'], title='Научные симуляторы')
+
+
 @app.route('/list_prof/<string:typeOfList>')
 def list_prof(typeOfList):
     listProf = ['инженер-исследователь',
